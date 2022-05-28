9ea012d04fc054d48c6e547064d506fef5f38355
C B Sajan <sajan@ckhathri.com>
2022-05-28 16:55:15 +0530

    Map Helpers

--- src/components/Admin/admin_comp.vue
+++ src/components/Admin/admin_comp.vue
@@ -1,48 +1,58 @@
 <template>
     <div class="row">
-        <h1>Admin</h1>
-        <hr />
-        <div class="col-sm-5">
-            <p>
-                <strong>
-                    Change count: {{ this.$store.state.counter }}
-                </strong>
-            </p>
+      <h1>Admin</h1>
+      <hr />
+      <div class="col-sm-5">
+        <p>
+          <strong>
+          Change count: {{ this.$store.getters.getCount }}
+          </strong>
+        </p>
 
-            <div>
-                <label class="form-label">By how much</label>
-                <input type="number" class="form-control" v-model="amount">
-            </div>
-            <br />
-
-            <button type="button" class="btn btn-primary" @click="adminAdd">
-                +
-            </button>
-            <button type="button" class="btn btn-outline-secondary" @click="adminSubstract">
-                -
-            </button>
+        <div>
+          <label class="form-label">By how much</label>
+          <input 
+            type="number" 
+            class="form-control" 
+            v-model="amount"
+          >
         </div>
+        <br/>
+
+        <button type="button" class="btn btn-primary" 
+        @click="adminAdd({ value: this.amount })">
+          +
+        </button>
+        <button type="button" class="btn btn-outline-secondary" @click="adminSubstract({ value: this.amount })">
+          -
+        </button>
+      </div>
     </div>
 </template>
 
 
 <script>
+import { mapMutations } from 'vuex';
 export default {
-    data() {
-        return {
-            amount: 1
-        }
+    data(){
+      return {
+        amount:1
+      }
     },
-    methods: {
-        adminAdd() {
-            this.$store.commit({
-                type: 'add',
-                value: this.amount
-            });
-        },
-        adminSubstract() {
-            this.$store.commit('subtract', { value: this.amount });
-        }
+    methods:{
+      ...mapMutations({
+        adminAdd:'add',
+        adminSubstract: 'substract'
+      }),
+      // adminAdd(){
+      //   this.$store.commit({
+      //     type:'add',
+      //     value: this.amount
+      //   });
+      // },
+      // adminSubstract(){
+      //   this.$store.commit('substract', { value:this.amount });
+      // }
     }
 }
 </script>
--- src/components/Counter/counter_comp.vue
+++ src/components/Counter/counter_comp.vue
@@ -1,39 +1,63 @@
 <template>
 
-    <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Count</h1>
-        <div class="col-lg-6 mx-auto">
-            <count-component></count-component>
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+<div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold">Count</h1>
+    <div class="col-lg-6 mx-auto">
+        <count-component></count-component>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 
-                <button type="button" class="btn btn-primary btn-lg px-4 gap-3" @click="add">+</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4" @click="subtract">-</button>
-                <hr />
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4" @click="getPrize">Get prize</button>
-            </div>
-        </div>
+        <button 
+          type="button" 
+          class="btn btn-primary btn-lg px-4 gap-3"
+          @click="add()"
+        >+</button>
+        <button 
+          type="button" 
+          class="btn btn-outline-secondary btn-lg px-4"
+          @click="substract()"
+        >-</button>
+        <hr/>
+        <button 
+          type="button" 
+          class="btn btn-outline-secondary btn-lg px-4"
+          @click="getPrize"
+        >Get prize</button>
+
+      </div>
     </div>
+  </div>
 
 </template>
 
 
 <script>
-import countComponent from './count_comp.vue'
+import countComponent from './count_comp.vue';
+import { mapGetters, mapMutations } from 'vuex'
 export default {
-    components: {
-        countComponent
-    },
-    methods: {
-        add() {
-            this.$store.commit('add');
-        },
-        subtract() {
-            this.$store.commit('subtract');
-        },
-        getPrize() {
-            const obj = this.$store.getters.getPrize;
-            alert(`Your prize: ${obj.prize}, in ${obj.attempts} attempt/s`)
-        }
+  components:{
+    countComponent
+  },
+  computed:{
+    ...mapGetters({
+      prize: 'getPrize'
+    }),
+    // ...mapGetters(['getPrize'])
+    // prize(){
+    //   return this.$store.getters.getPrize;
+    // },
+  },
+  methods:{
+    ...mapMutations(['add','substract']),
+    // add(){
+    //   this.$store.commit('add');
+    // },
+    // substract(){
+    //   this.$store.commit('substract');
+    // },
+    getPrize(){
+      const obj = this.prize;
+      alert(`Your prize: ${obj.prize}, in ${obj.attempts} attempt/s`)
     }
+  }
 }
 </script>
